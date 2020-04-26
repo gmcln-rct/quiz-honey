443faa86ab04469e2eb74c502f12c5ab4e62b28e
gmcln-rct <producersama@gmail.com>
2020-04-26 19:59:26 -0400

    Update Progress

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Progress(props) {
     return (
-        <div>
-               
-               
-        </div>
-    )
+        <h2>Question {props.current} of {props.total}</h2>
+
+    );
 }
+
+
