20fe39507c98f13c81c86e6839bb8a2d153c91d8
gmcln-rct <producersama@gmail.com>
2020-04-26 20:21:09 -0400

    Start Answers component

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 export default function Answer(props) {
     return (
-        <button value={props.letter}>
+        <button value={props.letter} className="answer">
             <span className="letter">
                 {props.letter}. {props.answer}
             </span>
