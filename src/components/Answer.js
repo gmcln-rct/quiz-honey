a43b6cf99470fe0d1567e1f3968eab1ee0f37d5a
gmcln-rct <producersama@gmail.com>
2020-05-02 15:13:23 -0400

    Update Answer selected

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 export default function Answer(props) {
+    let classes = ['answer'];
+
+    if (props.selected) {
+        classes.push('selected');
+    }
+
     return (
-        <button value={props.letter} className="answer">
+        <button value={props.letter} className={classes.join(' ')}>
             <span className="letter">
                 {props.letter}. {props.answer}
             </span>
--- src/components/Answers.js
+++ src/components/Answers.js
@@ -5,9 +5,9 @@ export default function Answers(props) {
     return (
         <>
             <Answer letter="a" answer={props.question.answer_a} selected={props.currentAnswer === 'a'} />
-            <Answer letter="b" answer={props.question.answer_b} selected={props.currentAnswer === 'a'} />
-            <Answer letter="c" answer={props.question.answer_c} selected={props.currentAnswer === 'a'} />
-            <Answer letter="d" answer={props.question.answer_d} selected={props.currentAnswer === 'a'} />
+            <Answer letter="b" answer={props.question.answer_b} selected={props.currentAnswer === 'b'} />
+            <Answer letter="c" answer={props.question.answer_c} selected={props.currentAnswer === 'c'} />
+            <Answer letter="d" answer={props.question.answer_d} selected={props.currentAnswer === 'd'} />
         </>
     );
 }
