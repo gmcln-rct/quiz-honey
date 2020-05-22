8102e8e4aa309ac3c81f7a773fdd77847db7245c
gmcln-rct <producersama@gmail.com>
2020-05-21 20:06:05 -0400

    Run new build

---
+++ build/precache-manifest.7a544d5505d5a1e7c74da53a00995136.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109d35dd165871585b2150c872be3c2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45a75f473385d072fdd1",
+    "url": "/static/css/main.6b9a8c58.chunk.css"
+  },
+  {
+    "revision": "f12f8d1d729ba563066f",
+    "url": "/static/js/2.54be4586.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.54be4586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a75f473385d072fdd1",
+    "url": "/static/js/main.3b3cd42e.chunk.js"
+  },
+  {
+    "revision": "be3702e3a369b996a57f",
+    "url": "/static/js/runtime-main.c87a5f44.js"
+  },
+  {
+    "revision": "736301b8a77552a29fe09a50f603c873",
+    "url": "/static/media/quiz-badger-logo.736301b8.png"
+  }
+]);
