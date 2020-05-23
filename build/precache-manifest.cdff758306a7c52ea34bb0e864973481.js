50cb386861d398ff9c92edbfecfc5fa5a420f247
gmcln-rct <producersama@gmail.com>
2020-05-23 19:49:56 -0400

    Add favicon

---
+++ build/precache-manifest.cdff758306a7c52ea34bb0e864973481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0131e2369ace72421d79d1070997fcb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "468abc22edb908e8fcc1",
+    "url": "/static/css/main.6b9a8c58.chunk.css"
+  },
+  {
+    "revision": "f12f8d1d729ba563066f",
+    "url": "/static/js/2.54be4586.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.54be4586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "468abc22edb908e8fcc1",
+    "url": "/static/js/main.6153e95b.chunk.js"
+  },
+  {
+    "revision": "be3702e3a369b996a57f",
+    "url": "/static/js/runtime-main.c87a5f44.js"
+  },
+  {
+    "revision": "736301b8a77552a29fe09a50f603c873",
+    "url": "/static/media/quiz-badger-logo.736301b8.png"
+  }
+]);
