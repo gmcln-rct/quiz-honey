f6ec961f95246448b3400e7ffbdf099a8e693a08
gmcln-rct <producersama@gmail.com>
2020-05-28 20:52:37 -0400

    Work on questions

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,8 +5,10 @@ export default function Logo() {
     return (
         <>
             <div className="logo-section">
-                <img src={logo} alt="Honey Badger Institute Logo" className="logo"></img>
-                <h3>Can you please The Badger?</h3>
+                <img src={logo} alt="Honey Badger Institute Logo" alt="Honey Badger Institute Logo" aria-label="Honey Badger Insitute Logo" className="logo"></img>
+                <h3>Can you please <span id="nowrap">
+                    The Badger?</span></h3>
+                
             </div>
         </>
     );
