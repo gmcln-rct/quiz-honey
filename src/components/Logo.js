9293bb05ef03a38671ecfe21251836572b08890c
gmcln-rct <producersama@gmail.com>
2020-06-08 20:33:39 -0400

    Add meta tags

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,7 @@ export default function Logo() {
     return (
         <>
             <div className="logo-section">
-                <img src={logo} alt="Honey Badger Institute Logo" alt="Honey Badger Institute Logo" aria-label="Honey Badger Insitute Logo" className="logo"></img>
+                <img src={logo} alt="Honey Badger Institute Logo" alt="Honey Badger Institute of Higher Learning Logo" aria-label="Honey Badger Institute of Higher Learning Logo" className="logo"></img>
                 <h3>Can you please <span id="nowrap">The Badger?</span></h3>
             </div>
         </>
